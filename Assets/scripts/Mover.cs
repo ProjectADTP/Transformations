484ad3735ab0e7ae1e6ae911ca579b8a1f6f85f8
Umage <38851250+ProjectADTP@users.noreply.github.com>
2025-02-25 00:32:41 +0300

    Add files via upload

---
+++ Assets/scripts/Mover.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * _speed);
+    }
+}
+
